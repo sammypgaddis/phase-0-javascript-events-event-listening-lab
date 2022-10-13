08a2d6a33ad56b9546ff2f76fef09318c7d2a8f6
Samuel Gaddis <siddag14@live.com>
2022-10-13 12:10:56 -0400

    commit tree 0c177e9cb76d97bdeb3137f5a1775f3c394938e8

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+function clickAlert() {
+    alert('hee hee that tickled');
 }
+input.addEventListener('click', clickAlert);  
+
+} //this is a way to write it if you were adding multiple elements to this event
+
+// const input = document.getElementById('button');
+// input.addEventListener('click', function() {
+//     alert('hee hee that tickled');
+// });
+//this is a way to write it if youre only calling that function in one place!!!
+
+
+
